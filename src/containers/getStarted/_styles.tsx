ce566a19e791d1ab7accb5e81b2e4c8030d246f1
webgently <gently039@gmail.com>
2024-05-20 03:09:15 +0800

    feat: add getStarted page

---
+++ src/containers/getStarted/_styles.tsx
@@ -0,0 +1,56 @@
+import { StyleSheet } from 'react-native';
+
+export default StyleSheet.create({
+  gSLayout: {
+    display: 'flex',
+    justifyContent: 'flex-end',
+    alignItems: 'center',
+    gap: 10,
+    width: '100%',
+    height: '100%',
+    paddingBottom: 40,
+    position: 'relative'
+  },
+  gSImg1: {
+    position: 'absolute',
+    top: 0,
+    left: 0,
+    zIndex: 0
+  },
+  gSImg2: {
+    position: 'absolute',
+    top: 50,
+    right: 0,
+    zIndex: 0
+  },
+  gSDescriptions: {
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
+    maxWidth: 250,
+    gap: 4
+  },
+  gSStepTitles: {
+    fontWeight: 'bold',
+    fontSize: 16,
+    color: '#2E2E5D'
+  },
+  gSStepDetails: {
+    textAlign: 'center',
+    fontSize: 14,
+    color: '#2E2E5D'
+  },
+  gSIndexs: {
+    display: 'flex',
+    flexDirection: 'row',
+    justifyContent: 'center',
+    gap: 10
+  },
+  gSController: {
+    display: 'flex',
+    flexDirection: 'row',
+    justifyContent: 'center',
+    width: '100%',
+    gap: 20
+  }
+});
--- src/containers/getStarted/index.tsx
+++ src/containers/getStarted/index.tsx
@@ -1,7 +1,83 @@
 import React from 'react';
+import { View, Text, Image } from 'react-native';
+import { AppButton } from '../../components/button';
+import styles from './_styles';
+
+const descriptions = [
+  { title: 'Title One', detail: 'Lorem ipsum dolor sit amet la maryame dor sut colondeum.' },
+  { title: 'Title Two', detail: 'Lorem ipsum dolor sit amet la maryame dor sut colondeum.' },
+  { title: 'Title Three', detail: 'Lorem ipsum dolor sit amet la maryame dor sut colondeum.' }
+];
 
 const getStarted = () => {
-  return <></>;
+  const [stepIndex, setStepIndex] = React.useState(0);
+
+  return (
+    <View style={styles.gSLayout}>
+      <Image source={require('../../assets/images/bgitem1.png')} style={styles.gSImg1} />
+      <Image source={require('../../assets/images/bgitem2.png')} style={styles.gSImg2} />
+      <View>
+        {stepIndex === 0 ? (
+          <Image source={require('../../assets/images/init1.png')} style={{ width: 220 }} />
+        ) : stepIndex === 1 ? (
+          <Image source={require(`../../assets/images/init2.png`)} style={{ width: 220 }} />
+        ) : (
+          <Image source={require(`../../assets/images/init3.png`)} style={{ width: 220 }} />
+        )}
+      </View>
+      <View style={styles.gSDescriptions}>
+        {descriptions.map((item, ind) => {
+          if (ind !== stepIndex) return;
+          return (
+            <React.Fragment key={ind}>
+              <Text style={styles.gSStepTitles}>{item.title}</Text>
+              <Text style={styles.gSStepDetails}>{item.detail}</Text>
+            </React.Fragment>
+          );
+        })}
+      </View>
+      <View style={styles.gSIndexs}>
+        {Array.from({ length: 3 }, (_, index) => {
+          return (
+            <Text
+              key={index}
+              style={{
+                color: stepIndex === index ? '#F77A55' : '#7466E3',
+                fontSize: 40,
+                fontWeight: 'bold'
+              }}>
+              &bull;
+            </Text>
+          );
+        })}
+      </View>
+      <View style={styles.gSController}>
+        {stepIndex === 0 || stepIndex === 1 ? (
+          <>
+            <AppButton
+              type="default"
+              title="Skip"
+              style={{ width: 140, height: 52 }}
+              onPress={() => stepIndex > 0 && setStepIndex(stepIndex - 1)}
+            />
+            <AppButton
+              type="primary"
+              title="Next"
+              style={{ width: 140, height: 52 }}
+              onPress={() => stepIndex < 2 && setStepIndex(stepIndex + 1)}
+            />
+          </>
+        ) : (
+          <AppButton
+            style={{ width: 280, height: 52 }}
+            type="primary"
+            title="Lets Get Started"
+            onPress={() => setStepIndex(0)}
+          />
+        )}
+      </View>
+    </View>
+  );
 };
 
 export default getStarted;
