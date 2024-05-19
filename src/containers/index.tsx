0fbba4ce3dbc906234a700e7deeaffb5656204c9
webgently <gently039@gmail.com>
2024-05-20 01:59:44 +0800

    feat: add demo function that redirect to any screen based on token validation

--- src/containers/index.tsx
+++ src/containers/index.tsx
@@ -1,7 +1,12 @@
-import React from 'react';
-import { View, Text } from 'react-native';
+import React, { useEffect } from 'react';
+import { CommonActions } from '@react-navigation/native';
+import { View } from 'react-native';
 
-const Containers = () => {
+const Containers = (props: any) => {
+  const token = '';
+  useEffect(() => {
+    props.navigation.dispatch(CommonActions.reset({ index: 0, routes: [{ name: token ? 'login' : 'get_started' }] }));
+  }, [token]);
   return <View />;
 };
 
