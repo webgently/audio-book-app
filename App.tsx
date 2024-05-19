8030b081f8d97793a88b89d35c4232cecf879043
webgently <gently039@gmail.com>
2024-05-19 20:12:15 +0800

    feat: inital router structure

--- App.tsx
+++ App.tsx
@@ -1,7 +1,21 @@
 import React from 'react';
+import { LogBox } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import Containers from './src/containers';
+
+LogBox.ignoreAllLogs();
+
+const Stack = createNativeStackNavigator();
 
 function App(): React.JSX.Element {
-  return <></>;
+  return (
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Initial">
+        <Stack.Screen name="Initial" component={Containers} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
 }
 
 export default App;
