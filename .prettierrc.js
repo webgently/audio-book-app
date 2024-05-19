5d163f22b25875d2a545da9e58505da42022d82d
webgently <gently039@gmail.com>
2024-05-19 10:37:20 +0800

    dep: prettier setting

--- .prettierrc.js
+++ .prettierrc.js
@@ -1,7 +1,11 @@
 module.exports = {
   arrowParens: 'avoid',
   bracketSameLine: true,
-  bracketSpacing: false,
+  bracketSpacing: true,
   singleQuote: true,
+  printWidth: 120,
+  useTabs: false,
+  tabWidth: 2,
+  semi: true,
   trailingComma: 'all',
 };
