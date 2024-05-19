7dd77c000a62c49abb8757b49b9e0423195097bd
webgently <gently039@gmail.com>
2024-05-19 20:00:43 +0800

    dep: installed module and add any code for navigation

--- android/app/src/main/java/com/audiobookapp/MainActivity.kt
+++ android/app/src/main/java/com/audiobookapp/MainActivity.kt
@@ -1,9 +1,9 @@
 package com.audiobookapp
-
 import com.facebook.react.ReactActivity
 import com.facebook.react.ReactActivityDelegate
 import com.facebook.react.defaults.DefaultNewArchitectureEntryPoint.fabricEnabled
 import com.facebook.react.defaults.DefaultReactActivityDelegate
+import android.os.Bundle;
 
 class MainActivity : ReactActivity() {
 
@@ -12,6 +12,10 @@ class MainActivity : ReactActivity() {
    * rendering of the component.
    */
   override fun getMainComponentName(): String = "AudioBookApp"
+
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(null)
+  }
 
   /**
    * Returns the instance of the [ReactActivityDelegate]. We use [DefaultReactActivityDelegate]
