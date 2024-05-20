51f4a2c9f38669cb27a228d66133de86378acf9c
webgently <gently039@gmail.com>
2024-05-21 03:11:31 +0800

    feat: add custome checkbox

---
+++ src/components/checkbox.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import { CheckedIcon, UnCheckedIcon } from './icons';
+import { TouchableOpacity } from 'react-native';
+
+const CheckBox = ({ selected, onPress, style }: any) => (
+  <TouchableOpacity style={[{ flexDirection: 'row', alignItems: 'center' }, style]} onPress={onPress}>
+    {selected ? <CheckedIcon line="#BBB1FA" bg="#4838D1" /> : <UnCheckedIcon color="#BBB1FA" />}
+  </TouchableOpacity>
+);
+
+export default CheckBox;
