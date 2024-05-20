fa721173d3ffecf2c71fbc5174388db48bcc538b
webgently <gently039@gmail.com>
2024-05-20 15:14:39 +0800

    feat: add new router and update structure

--- App.tsx
+++ App.tsx
@@ -3,7 +3,9 @@ import { LogBox, StatusBar } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import Containers from './src/containers';
-import getStarted from './src/containers/getStarted';
+import GetStarted from './src/containers/getStarted';
+import Auth from './src/containers/auth';
+import HomePage from './src/containers/homePage';
 
 LogBox.ignoreAllLogs();
 
@@ -15,7 +17,9 @@ function App(): React.JSX.Element {
       <StatusBar hidden={true} />
       <Stack.Navigator initialRouteName="initial">
         <Stack.Screen name="initial" component={Containers} options={{ headerShown: false }} />
-        <Stack.Screen name="get_started" component={getStarted} options={{ headerShown: false }} />
+        <Stack.Screen name="get_started" component={GetStarted} options={{ headerShown: false }} />
+        <Stack.Screen name="auth" component={Auth} options={{ headerShown: false }} />
+        <Stack.Screen name="home_page" component={HomePage} options={{ headerShown: false }} />
       </Stack.Navigator>
     </NavigationContainer>
   );
