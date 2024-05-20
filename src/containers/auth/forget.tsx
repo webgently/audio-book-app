ee9fea1103991f74b6acabd2712ce254e722bf0a
webgently <gently039@gmail.com>
2024-05-21 03:41:17 +0800

    feat: init forget screen and register screen

---
+++ src/containers/auth/forget.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import { Text } from 'react-native';
+
+const Forget = ({ toRedirect }: any) => {
+  return <Text>forget</Text>;
+};
+
+export default Forget;
---
+++ src/containers/auth/register.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import { Text } from 'react-native';
+
+const Register = ({ toRedirect }: any) => {
+  return <Text>register</Text>;
+};
+
+export default Register;
