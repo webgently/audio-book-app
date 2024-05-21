589935c3ee88d9b8584bbe8b092b630dd7f01931
webgently <gently039@gmail.com>
2024-05-21 15:09:25 +0800

    update: auth layout

--- src/containers/auth/index.tsx
+++ src/containers/auth/index.tsx
@@ -3,16 +3,20 @@ import { ScrollView, View, Text, Image } from 'react-native';
 import Login from './login';
 import Register from './register';
 import Forget from './forget';
+import Sent from './sent';
+import Confirm from './confirm';
 import styles from './_styles';
 
-const titles = ['Login to Your Account', 'Register', 'Forget Password', 'Confirmation Code', 'Email Sent'];
+const titles = ['Login to Your Account', 'Register', 'Forget Password', 'Email Sent', 'Confirmation Code'];
 
 const Auth = () => {
   const [stepIndex, setStepIndex] = React.useState<number>(0);
   const screens = [
     <Login toRedirect={setStepIndex} />,
     <Register toRedirect={setStepIndex} />,
-    <Forget toRedirect={setStepIndex} />
+    <Forget toRedirect={setStepIndex} />,
+    <Sent toRedirect={setStepIndex} />,
+    <Confirm toRedirect={setStepIndex} />
   ];
 
   return (
